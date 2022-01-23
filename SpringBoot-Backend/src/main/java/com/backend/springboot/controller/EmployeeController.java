c469d5fb04f46f92cd4165ecf062633a245b49cd
Anurag Tiwari <anurag.7140@gmail.com>
2022-01-23 21:02:26 +0530

    Create Update Employee REST API

--- SpringBoot-Backend/src/main/java/com/backend/springboot/controller/EmployeeController.java
+++ SpringBoot-Backend/src/main/java/com/backend/springboot/controller/EmployeeController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -20,28 +21,41 @@ import com.backend.springboot.repository.EmployeeRepository;
 @RestController
 @RequestMapping("/api/v1/")
 public class EmployeeController {
-	
+
 	@Autowired
 	private EmployeeRepository employeeRepository;
 
 	@GetMapping("/employees")
-	public List<Employee> getAllEmployees(){
+	public List<Employee> getAllEmployees() {
 		return employeeRepository.findAll();
 	}
-	
+
 	// create Employee Rest Api
 	@PostMapping("employees")
 	@CrossOrigin(origins = "http://localhost:4200")
 	public Employee createEmployee(@RequestBody Employee employee) {
 		return employeeRepository.save(employee);
 	}
-	
-	
-	//Get Employee by id rest API
-	@GetMapping("employees/{id}")
+
+	// Get Employee by id rest API
+	@GetMapping("/employees/{id}")
 	public ResponseEntity<Employee> getEmployeeByID(@PathVariable Long id) {
-		Employee employee = employeeRepository.findById(id).orElseThrow(()-> new ResourceNotFoundException("Employee Not exist with ID : "+id));
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee Not exist with ID : " + id));
 		return ResponseEntity.ok(employee);
 	}
-	
+
+	// EDIT Employee Rest API
+	@PutMapping("/employees/{id}")
+	public ResponseEntity<Employee> editEmployees(@PathVariable Long id, @RequestBody Employee employeeDetails){
+		Employee employee = employeeRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Employee Not exist with ID : " + id));
+		employee.setFirstName(employeeDetails.getFirstName());
+		employee.setLastName(employeeDetails.getLastName());
+		employee.setEmail(employeeDetails.getEmail());
+		
+		employeeRepository.save(employee);
+		return ResponseEntity.ok(employee);
+	}
+
 }
